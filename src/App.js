327445723b10d681ed2ec35f9da64b62e09283f2
Donald Ballowe <dballowe7912@gmail.com>
2021-02-08 02:07:32 -0600

    Disabled buttons and displayed losing message after max guesses reached

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Hangman />
+        <Hangman maxGuesses={6}/>
       </div>
     );
   }
--- src/Hangman.js
+++ src/Hangman.js
@@ -46,6 +46,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -61,8 +62,12 @@ class Hangman extends Component {
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
+        <p>Gussed Wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        { this.state.nWrong < this.props.maxGuesses ? 
+          <p className='Hangman-btns'>{this.generateButtons()}</p> 
+          : <p>Sorry You Lose!</p>
+        }
       </div>
     );
   }
